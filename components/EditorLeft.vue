ef03600bc8fba766b21865fc0015bea3ad7f5fd9
IKaRos <w71800@gmail.com>
2024-10-11 11:40:23 +0800

    feat: 加上推薦好友下拉選單的基本樣式與功能

--- components/EditorLeft.vue
+++ components/EditorLeft.vue
@@ -1,23 +1,29 @@
 <!-- 
   @todo:
   - 如何將這個地方也吃到一些 Editor 中的共用樣式？
+  - 如何讓推薦參與者列表展開？
+  - 點擊推薦參與者時，如何將其加入到 chosenParticipants 中？
 -->
 
 <template lang="pug">
 .scroll-contents__left
   Topbar(:left="topbarConfig.left" :middle="topbarConfig.middle" :right="topbarConfig.right")
   .editor__participants
-  .participants-list
-    .label 和：
-    .participants-item(v-for="participant in chosenParticipants" :key="participant.id") 
-      .avatar
-        img(:src="participant.avatar")
-      .name {{ participant.displayName }}
-      .remove
-        img(:src="'/icons/remove.png'")
-  .recommend-participants
-    .recommend-item(v-for="participant in recommendParticipants" :key="participant.id") {{ participant.displayName }}
-  .test(@click="isEditorScrolled = true") 到下頁
+    .participants-list
+      .label 和：
+      .participants-item(v-for="participant in chosenParticipants" :key="participant.id") 
+        .avatar
+          img(:src="participant.avatar")
+        .name {{ participant.displayName }}
+        .remove
+          img(:src="'/icons/remove.png'")
+    .recommend-participants(:class="{ 'active': isRecommendListActive }")
+      .recommend-item(v-for="participant in recommendParticipants" :key="participant.id") 
+        .avatar
+          img(:src="participant.avatar")
+        span {{ participant.displayName }}
+  .editor__body
+  .editor__footer
 </template>
 
 <script setup lang="ts">
@@ -26,6 +32,7 @@ import type { Participant } from '@/types/types'
 
 const isEditorShowing = inject('isEditorShowing') as Ref<boolean>
 const isEditorScrolled = inject('isEditorScrolled') as Ref<boolean>
+const isRecommendListActive = ref(false)
 
 // 假資料：已選擇的參與者
 const chosenParticipants = ref<Participant[]>([
@@ -43,25 +50,6 @@ const chosenParticipants = ref<Participant[]>([
   }
 ])
 
-// 假資料：推薦的參與者
-const recommendParticipants = ref<Participant[]>([
-  {
-    id: '3',
-    displayName: '王五',
-    avatar: '/avatars/profile.jpg'
-  },
-  {
-    id: '4',
-    displayName: '趙六',
-    email: 'zhaoliu@example.com',
-    avatar: '/avatars/profile.jpg'
-  },
-  {
-    id: '5',
-    displayName: '孫七',
-    avatar: '/avatars/profile.jpg'
-  }
-])
 
 const topbarConfig = {
   left: {
@@ -81,6 +69,7 @@ const topbarConfig = {
 </script>
 
 <style lang="sass" scoped>
+// todo: 調整 topbar 的樣式，這樣太醜了
 .topbar
   position: absolute
   top: -44px
@@ -101,37 +90,58 @@ const topbarConfig = {
 
 .editor
   &__participants
-  .participants-list
-    align-items: center
-    display: flex
-    gap: 10px
-    padding: 10px 16px
-    border-top: 2px solid rgba(#929292, 0.3)
-    border-bottom: 2px solid rgba(#929292, 0.3)
-  .label
-    color: #D5D5D5
-    font-weight: $font-weight-bold
-  .participants-item
-    align-items: center
-    display: flex
-    justify-content: space-between
-    gap: 10px
-    margin-right: 8px
-    border: 1px solid #929292
-    border-radius: 24px
-    padding: 6px 16px 6px 10px
-    .avatar
-      +block_size(24px)
-      border: 1px solid #000
-    .name
-      color: #929292
-    .remove
-      +block_size(13px)
-      cursor: pointer
-      opacity: 0.3
-      font-size: 0
-      img
-        width: 100%
-        height: 100%
-        object-fit: cover
+    .participants-list
+      align-items: center
+      display: flex
+      gap: 10px
+      padding: 10px 16px
+      border-top: 2px solid rgba(#929292, 0.3)
+      border-bottom: 2px solid rgba(#929292, 0.3)
+    .label
+      color: #D5D5D5
+      font-weight: $font-weight-bold
+    .participants-item
+      align-items: center
+      display: flex
+      justify-content: space-between
+      gap: 10px
+      margin-right: 8px
+      border: 1px solid #929292
+      border-radius: 24px
+      padding: 6px 16px 6px 10px
+      .avatar
+        +block_size(24px)
+        border: 1px solid #000
+      .name
+        color: #929292
+      .remove
+        +block_size(13px)
+        cursor: pointer
+        opacity: 0.3
+        font-size: 0
+        img
+          width: 100%
+          height: 100%
+          object-fit: cover
+    
+    // todo: 加上展開時的過渡效果
+    .recommend-participants
+      height: 0
+      overflow: hidden
+      transition: height 0.3s ease
+      .recommend-item
+        display: flex
+        align-items: center
+        gap: 10px
+        border-bottom: 1px solid #D5D5D5
+        padding: 10px 16px
+      .avatar
+        border: 1px solid #929292
+      span
+        color: #929292
+
+
+.recommend-participants
+  &.active
+    height: auto
 </style>
