a61080cc15d340b3675afa1266aaff0b4a8c14ba
IKaRos <w71800@gmail.com>
2024-10-23 16:49:50 +0800

    chore: 加上 uuid 功能

--- store/editor.ts
+++ store/editor.ts
@@ -1,9 +1,36 @@
 import { defineStore } from 'pinia'
-import type { Record, Participant, Division, Payer } from '@/types/types'
+import type { Record } from '@/types/types'
+import { useUserDataStore } from '@/store/userData'
+import { v4 as uuidv4 } from 'uuid'
+
+
+const createEmptyRecord = (): Record => {
+  const userDataStore = useUserDataStore()
+  const userData = {
+    id: userDataStore.id,
+    displayName: userDataStore.displayName,
+    email: userDataStore.email,
+    avatar: userDataStore.avatar
+  }
+
+
+  return {
+    id: uuidv4(),
+    title: '',
+    value: 0,
+    currency: '',
+    fullDate: new Date().toISOString().split('T')[0],
+    participants: [ userData ],
+    divisions: [ { ...userData, value: 0 } ],
+    payers: { ...userData, paid: 0 },
+    splitor: 'equal',
+    group: null
+  }
+}
 
 export const useEditorStore = defineStore('editor', {
   state: () => ({
-    record: {} as Record
+    record: createEmptyRecord()
   }),
   actions: {
     setRecord(record: Record) {
@@ -11,67 +38,3 @@ export const useEditorStore = defineStore('editor', {
     }
   }
 })
-
-// id: 'rec123456',
-//       title: '週末聚餐',
-//       value: 1200,
-//       currency: 'TWD',
-//       fullDate: '2023-04-15',
-//       participants: [
-//         {
-//           id: 'user1',
-//           displayName: '小明',
-//           tags: ['朋友']
-//         },
-//         {
-//           id: 'user2',
-//           displayName: '小華',
-//           tags: ['同事']
-//         },
-//         {
-//           id: 'user3',
-//           displayName: '小李',
-//           tags: ['家人']
-//         }
-//       ] as Participant[],
-//       divisions: [
-//         {
-//           id: 'user1',
-//           displayName: '小明',
-//           value: 400
-//         },
-//         {
-//           id: 'user2',
-//           displayName: '小華',
-//           value: 400
-//         },
-//         {
-//           id: 'user3',
-//           displayName: '小李',
-//           value: 400
-//         }
-//       ] as Division[],
-//       payers: {
-//         id: 'user1',
-//         displayName: '小明',
-//         paid: 1200
-//       } as Payer,
-//       splitor: 'equal',
-//       group: {
-//         id: 'group1',
-//         name: '好朋友群組',
-//         members: [
-//           {
-//             id: 'user1',
-//             displayName: '小明'
-//           },
-//           {
-//             id: 'user2',
-//             displayName: '小華'
-//           },
-//           {
-//             id: 'user3',
-//             displayName: '小李'
-//           }
-//         ]
-//       }
