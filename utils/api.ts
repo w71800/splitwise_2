190b30890445ff62739b3b26958c539b89e05159
IKaRos <w71800@gmail.com>
2024-11-07 15:02:01 +0800

    style: 更結構化地撰寫 strapi 的 populate params

--- utils/api.ts
+++ utils/api.ts
@@ -1,41 +1,49 @@
-// export const fetchRecords = (source: 'groups' | 'friends', id: string | number) => {
-//   return fetch(`/api/records?source=${source}&id=${id}`)
-//     .then( (res) => {
-//       if(!res.ok) {
-//         throw new Error('Failed to fetch records')
-//       }
-//       return res.json()
-//     })
-//     .catch( (error) => {
-//       throw new Error(error.message)
-//     })
-// }
-
-const url = 'http://localhost:1337/api/records?populate[payers][populate][payer][fields]=id,documentId,email,username&populate[participants][populate][participant][fields]=id,documentId,email,username&populate[participants][populate]=tags&populate[divisions][populate][participant][fields]=id,documentId,email,username'
 export const fetchRecordDatas = () => {
   const config = useRuntimeConfig()
+  const { strapiHost } = config.public
 
-  return fetch(url, {
+  const populateParams = [
+    'populate[participants][populate][participant][populate][avatar][fields]=url',
+    'populate[participants][populate]=tags',
+    'populate[payers][populate][payer][populate][avatar][fields]=url',
+    'populate[divisions][populate][participant][populate][avatar][fields]=url',
+    'populate[group][populate][members][fields]=id, documentId, username, email',
+    'populate[group][populate][members][populate][avatar][fields]=url'
+  ].join('&')
+
+  const endpoint = `${strapiHost}/api/records?${populateParams}`
+
+  return fetch(endpoint, {
     headers: {
       Authorization: `Bearer ${config.public.strapiTokenDev}`
     }
   })
-    .then( (res) => {
-      if(!res.ok) {
+    .then((res) => {
+      if (!res.ok) {
         throw new Error('Failed to fetch records')
       }
       return res.json().then(data => data.data)
     })
-    .catch( (error) => {
+    .catch((error) => {
       throw new Error(error.message)
     })
 }
 
 export const fetchUserData = () => {
-  const runtimeConfig = useRuntimeConfig()
-  
-  const token = useCookie('token').value || runtimeConfig.public.strapiUserToken
-  const endpoint = `http://localhost:1337/api/users/me?populate[friends][populate]=*&populate[groups][populate]=*&populate[avatar][fields]=url`
+  const config = useRuntimeConfig()
+  const { strapiHost } = config.public
+  const token = useCookie('token').value || config.public.strapiUserToken
+
+  const populateParams = [
+    'populate[friends][populate]=*',
+    'populate[groups][populate]=*',
+    'populate[avatar][fields]=url',
+    'populate[groups][populate][members][populate][avatar][fields]=url',
+    'populate[groups][populate][avatar][fields]=url'
+  ].join('&')
+
+  const endpoint = `${strapiHost}/api/users/me?${populateParams}`
+
   return fetch(endpoint, {
     headers: {
       Authorization: `Bearer ${token}`
