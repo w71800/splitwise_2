2033384d20e846cd7321f9cc75867763ab5233ac
IKaRos <w71800@gmail.com>
2024-11-29 15:46:01 +0800

    chore: 安裝了 Vuelidate

---
+++ components/Auth.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  form(@submit.prevent="handleSubmit")
+
+</template>
+
+<script setup lang="ts">
+const isSignup = ref(false) // 如果是註冊的話，會額外多一個暱稱欄位
+
+</script>
+
+<style scoped lang="sass">
+
+</style>
---
+++ pages/auth/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.page
+  .background
+  Auth
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="sass">
+.page
+  position: absolute
+  width: 100vw
+  height: 100vh
+  left: 0
+  top: 0
+  backdrop-filter: blur(10px)
+  z-index: 1000
+</style>
