1184e9605b25c9bd625a9611597eaae14f6a68dd
IKaRos <w71800@gmail.com>
2025-02-22 15:11:24 +0800

    chore: 調整 nuxt 模式為 CSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   alias: {
