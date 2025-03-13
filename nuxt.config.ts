ae3eabec24190996dba98314d357f46c4e3bcc93
IKaRos <w71800@gmail.com>
2025-03-13 17:51:48 +0800

    chore: 加上了 titltlle 跟 favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@ import { resolve } from 'path'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Self-Made Splitwise',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/auth/logo.png'
+        }
+      ]
+    }
+  },
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
